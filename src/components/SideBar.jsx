fdf98deea0be33581383b5335509eaac290ec695
midaismael <midaismael@gmail.com>
2025-02-21 23:32:54 +0200

    fonts, router, material ui

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,330 @@
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
+import List from "@mui/material/List";
+import MuiDrawer from "@mui/material/Drawer";
+import { Avatar, styled, Typography, useTheme } from "@mui/material";
+import {
+  BarChartOutlined,
+  CalendarTodayOutlined,
+  ContactsOutlined,
+  HelpOutlineOutlined,
+  HomeOutlined,
+  MapOutlined,
+  PeopleOutline,
+  PersonOutline,
+  PieChartOutlineOutlined,
+  ReceiptOutlined,
+  TimelineOutlined,
+} from "@mui/icons-material";
+import { useNavigate } from "react-router-dom";
+
+const drawerWidth = 240;
+
+const openedMixin = (theme) => ({
+  width: drawerWidth,
+  transition: theme.transitions.create("width", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.enteringScreen,
+  }),
+  overflowX: "hidden",
+});
+
+const closedMixin = (theme) => ({
+  transition: theme.transitions.create("width", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  overflowX: "hidden",
+  width: `calc(${theme.spacing(7)} + 1px)`,
+  [theme.breakpoints.up("sm")]: {
+    width: `calc(${theme.spacing(8)} + 1px)`,
+  },
+});
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "flex-end",
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+}));
+
+const Drawer = styled(MuiDrawer, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme }) => ({
+  width: drawerWidth,
+  flexShrink: 0,
+  whiteSpace: "nowrap",
+  boxSizing: "border-box",
+  variants: [
+    {
+      props: ({ open }) => open,
+      style: {
+        ...openedMixin(theme),
+        "& .MuiDrawer-paper": openedMixin(theme),
+      },
+    },
+    {
+      props: ({ open }) => !open,
+      style: {
+        ...closedMixin(theme),
+        "& .MuiDrawer-paper": closedMixin(theme),
+      },
+    },
+  ],
+}));
+
+const firstArray = [
+  { text: "Dashboard", icon: <HomeOutlined />, path: "/" },
+  { text: "Manage Team", icon: <PeopleOutline />, path: "/team" },
+  {
+    text: "Contacts Information",
+    icon: <ContactsOutlined />,
+    path: "/contacts",
+  },
+  { text: "Invoics Balances", icon: <ReceiptOutlined />, path: "/invoices" },
+];
+
+const secondArray = [
+  { text: "Profile Form", icon: <PersonOutline />, path: "/form" },
+  { text: "Calendar", icon: <CalendarTodayOutlined />, path: "/calendar" },
+  { text: "FAQ Page", icon: <HelpOutlineOutlined />, path: "/faq" },
+];
+
+const thirdArray = [
+  { text: "Bar Chart", icon: <BarChartOutlined />, path: "/bar" },
+  { text: "Pie Chart", icon: <PieChartOutlineOutlined />, path: "/pie" },
+  {
+    text: "Line Chart",
+    icon: <TimelineOutlined />,
+    path: "/line",
+  },
+  { text: "Geography Chart", icon: <MapOutlined />, path: "/geography" },
+];
+
+export default function SideBar({ open, handleDrawerClose }) {
+  const theme = useTheme();
+  const navigate = useNavigate();
+  return (
+    <Drawer variant="permanent" open={open}>
+      <DrawerHeader>
+        <IconButton onClick={handleDrawerClose}>
+          {theme.direction === "rtl" ? (
+            <ChevronRightIcon />
+          ) : (
+            <ChevronLeftIcon />
+          )}
+        </IconButton>
+      </DrawerHeader>
+      <Divider />
+
+      <Avatar
+        sx={{
+          mx: "auto",
+          width: open ? 90 : 45,
+          height: open ? 90 : 45,
+          my: 1,
+          transition: "0.25s",
+          border: "2px solid gray",
+        }}
+        alt="Remy Sharp"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAL16V6Exfl9MoblYTP4J6BsvyE19sCzltxw&s"
+      />
+      <Typography
+        align="center"
+        sx={{ fontSize: open ? 17 : 0, transition: "0.25s" }}
+      >
+        Ahmed
+      </Typography>
+      <Typography
+        align="center"
+        sx={{
+          fontSize: open ? 15 : 0,
+          transition: "0.25s",
+          color: theme.palette.info.main,
+        }}
+      >
+        Admin
+      </Typography>
+      <Divider />
+
+      <List>
+        {firstArray.map((item) => (
+          <ListItem key={item.text} disablePadding sx={{ display: "block" }}>
+            <ListItemButton
+              onClick={() => {
+                navigate(item.path);
+              }}
+              sx={[
+                {
+                  minHeight: 48,
+                  px: 2.5,
+                },
+                open
+                  ? {
+                      justifyContent: "initial",
+                    }
+                  : {
+                      justifyContent: "center",
+                    },
+              ]}
+            >
+              <ListItemIcon
+                sx={[
+                  {
+                    minWidth: 0,
+                    justifyContent: "center",
+                  },
+                  open
+                    ? {
+                        mr: 3,
+                      }
+                    : {
+                        mr: "auto",
+                      },
+                ]}
+              >
+                {item.icon}
+              </ListItemIcon>
+              <ListItemText
+                primary={item.text}
+                sx={[
+                  open
+                    ? {
+                        opacity: 1,
+                      }
+                    : {
+                        opacity: 0,
+                      },
+                ]}
+              />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+
+      <Divider />
+      <List>
+        {secondArray.map((item) => (
+          <ListItem key={item.text} disablePadding sx={{ display: "block" }}>
+            <ListItemButton
+              onClick={() => {
+                navigate(item.path);
+              }}
+              sx={[
+                {
+                  minHeight: 48,
+                  px: 2.5,
+                },
+                open
+                  ? {
+                      justifyContent: "initial",
+                    }
+                  : {
+                      justifyContent: "center",
+                    },
+              ]}
+            >
+              <ListItemIcon
+                sx={[
+                  {
+                    minWidth: 0,
+                    justifyContent: "center",
+                  },
+                  open
+                    ? {
+                        mr: 3,
+                      }
+                    : {
+                        mr: "auto",
+                      },
+                ]}
+              >
+                {item.icon}
+              </ListItemIcon>
+              <ListItemText
+                primary={item.text}
+                sx={[
+                  open
+                    ? {
+                        opacity: 1,
+                      }
+                    : {
+                        opacity: 0,
+                      },
+                ]}
+              />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+
+      <Divider />
+      <List>
+        {thirdArray.map((item) => (
+          <ListItem key={item.text} disablePadding sx={{ display: "block" }}>
+            <ListItemButton
+              onClick={() => {
+                navigate(item.path);
+              }}
+              sx={[
+                {
+                  minHeight: 48,
+                  px: 2.5,
+                },
+                open
+                  ? {
+                      justifyContent: "initial",
+                    }
+                  : {
+                      justifyContent: "center",
+                    },
+              ]}
+            >
+              <ListItemIcon
+                sx={[
+                  {
+                    minWidth: 0,
+                    justifyContent: "center",
+                  },
+                  open
+                    ? {
+                        mr: 3,
+                      }
+                    : {
+                        mr: "auto",
+                      },
+                ]}
+              >
+                {item.icon}
+              </ListItemIcon>
+              <ListItemText
+                primary={item.text}
+                sx={[
+                  open
+                    ? {
+                        opacity: 1,
+                      }
+                    : {
+                        opacity: 0,
+                      },
+                ]}
+              />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+
+      <Divider />
+    </Drawer>
+  );
+}
