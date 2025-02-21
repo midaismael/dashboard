f6baa3cc30a8eaf30ce8f24dee4cbe93bee7e09f
midaismael <midaismael@gmail.com>
2025-02-21 23:33:31 +0200

    starting project

---
+++ src/App.jsx
@@ -0,0 +1,63 @@
+import * as React from "react";
+import { createTheme, styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Typography from "@mui/material/Typography";
+import TopBar from "./components/TopBar";
+import SideBar from "./components/SideBar";
+import { ThemeProvider } from "@mui/material/styles";
+import { getDesignTokens } from "./theme";
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "flex-end",
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+}));
+
+export default function MiniDrawer() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  const [mode, setMode] = React.useState(
+    Boolean(localStorage.getItem("currentMode"))
+      ? localStorage.getItem("currentMode")
+      : "light"
+  );
+  const theme = React.useMemo(() => createTheme(getDesignTokens(mode)), [mode]);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Box sx={{ display: "flex" }}>
+        <CssBaseline />
+
+        {/* top bar nav */}
+        <TopBar
+          open={open}
+          handleDrawerOpen={handleDrawerOpen}
+          setMode={setMode}
+        />
+        {/* top bar nav */}
+
+        {/* side bar nav */}
+        <SideBar open={open} handleDrawerClose={handleDrawerClose} />
+        {/* side bar nav */}
+
+        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+          <DrawerHeader />
+          <Typography sx={{ marginBottom: 2 }}>Lorem ipsum</Typography>
+        </Box>
+      </Box>
+    </ThemeProvider>
+  );
+}
