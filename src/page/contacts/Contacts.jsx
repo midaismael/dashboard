9a7804344949e6ee180c368fff7858e2c6086908
midaismael <midaismael@gmail.com>
2025-02-23 20:18:47 +0200

    contacts page

--- src/page/contacts/Contacts.jsx
+++ src/page/contacts/Contacts.jsx
@@ -1,5 +1,29 @@
-import React from "react";
+import { Box, Typography } from "@mui/material";
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+import { useTheme } from "@mui/material";
+import { rows, columns } from "./data";
 
 export default function Contacts() {
-  return <div>Contacts</div>;
+  const theme = useTheme();
+  return (
+    <Box sx={{ height: 600, width: "98%", mx: "auto" }}>
+      <Typography
+        sx={{
+          fontSize: "30px",
+          letterSpacing: "2px",
+          fontWeight: "bold",
+          color: theme.palette.primary.dark,
+          fontFamily: "circular-web",
+        }}
+      >
+        CONTACTS
+      </Typography>
+      <DataGrid
+        slots={{ toolbar: GridToolbar }}
+        rows={rows}
+        // @ts-ignore
+        columns={columns}
+      />
+    </Box>
+  );
 }
