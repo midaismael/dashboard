1880dde76ca2746960854acce9309094c1837a39
midaismael <midaismael@gmail.com>
2025-02-23 20:34:52 +0200

    invoices page

--- src/page/invoices/Invoices.jsx
+++ src/page/invoices/Invoices.jsx
@@ -1,5 +1,29 @@
-import React from "react";
+import { Box, Typography, useTheme } from "@mui/material";
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+import { rows, columns } from "./data";
 
 export default function Invoices() {
-  return <div>Invoices</div>;
+  const theme = useTheme();
+  return (
+    <Box sx={{ height: 600, width: "98%", mx: "auto" }}>
+      <Typography
+        sx={{
+          fontSize: "30px",
+          letterSpacing: "2px",
+          fontWeight: "bold",
+          color: theme.palette.primary.dark,
+          fontFamily: "circular-web",
+        }}
+      >
+        CONTACTS
+      </Typography>
+      <DataGrid
+        checkboxSelection
+        slots={{ toolbar: GridToolbar }}
+        rows={rows}
+        // @ts-ignore
+        columns={columns}
+      />
+    </Box>
+  );
 }
