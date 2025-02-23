37f6e7bce468260ace6332b3106edbbfdd62591b
midaismael <midaismael@gmail.com>
2025-02-23 22:50:59 +0200

    form page

--- src/page/form/Form.jsx
+++ src/page/form/Form.jsx
@@ -1,5 +1,183 @@
+import {
+  Alert,
+  Button,
+  MenuItem,
+  Snackbar,
+  Stack,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
 import React from "react";
+import { useForm } from "react-hook-form";
+
+const regEmail =
+  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+const phoneRegExp =
+  /^((\+[1-9]{1,4}[ -]?)|(\([0-9]{2,3}\)[ -]?)|([0-9]{2,4})[ -]?)*?[0-9]{3,4}[ -]?[0-9]{3,4}$/;
+
+const data = [
+  {
+    value: "Admin",
+    label: "Admin",
+  },
+  {
+    value: "Manger",
+    label: "Manger",
+  },
+  {
+    value: "User",
+    label: "User",
+  },
+];
 
 export default function Form() {
-  return <div>Form</div>;
+  const theme = useTheme();
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = () => {
+    handleClick();
+    console.log("donnnnnnne");
+  };
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleClick = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
+  };
+  return (
+    <Box
+      onSubmit={handleSubmit(onSubmit)}
+      component="form"
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        gap: 3,
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <Typography
+        sx={{
+          fontSize: "30px",
+          letterSpacing: "2px",
+          fontWeight: "bold",
+          color: theme.palette.primary.dark,
+          fontFamily: "circular-web",
+        }}
+      >
+        CREATE USER
+      </Typography>
+      <Stack direction={"row"} sx={{ gap: 2 }}>
+        <TextField
+          error={Boolean(errors.firstName)}
+          helperText={
+            Boolean(errors.firstName)
+              ? "This field is required & min 3 character"
+              : ""
+          }
+          {...register("firstName", { required: true, minLength: 3 })}
+          sx={{ flex: 1 }}
+          label="First Name"
+          variant="filled"
+        />
+        <TextField
+          error={Boolean(errors.lastName)}
+          helperText={
+            Boolean(errors.lastName)
+              ? "This field is required & min 3 character"
+              : null
+          }
+          {...register("lastName", { required: true, minLength: 3 })}
+          sx={{ flex: 1 }}
+          label="Last Name"
+          variant="filled"
+        />
+      </Stack>
+
+      <TextField
+        error={Boolean(errors.email)}
+        helperText={
+          Boolean(errors.email) ? "Please provide a valid email address" : null
+        }
+        {...register("email", {
+          required: true,
+          pattern: regEmail,
+        })}
+        label="Email"
+        variant="filled"
+      />
+
+      <TextField
+        error={Boolean(errors.contactNumber)}
+        helperText={
+          Boolean(errors.contactNumber)
+            ? "Please provide a valid phone number"
+            : null
+        }
+        {...register("contactNumber", {
+          required: true,
+          pattern: phoneRegExp,
+        })}
+        label="Contact Number"
+        variant="filled"
+      />
+      <TextField label="Adress 1" variant="filled" />
+      <TextField label="Adress 2" variant="filled" />
+      <TextField
+        id="outlined-select-currency"
+        select
+        label="Role"
+        defaultValue="User"
+        helperText="Please select your role"
+      >
+        {data.map((option) => (
+          <MenuItem key={option.value} value={option.value}>
+            {option.label}
+          </MenuItem>
+        ))}
+      </TextField>
+      <Box sx={{ textAlign: "right " }}>
+        <Button
+          type="submit"
+          sx={{
+            textTransform: "capitalize",
+            fontFamily: "general",
+          }}
+          variant="contained"
+        >
+          Create New User
+        </Button>
+        <Snackbar
+          anchorOrigin={{ vertical: "top", horizontal: "right" }}
+          open={open}
+          autoHideDuration={3000}
+          onClose={handleClose}
+        >
+          <Alert
+            onClose={handleClose}
+            severity="success"
+            variant="filled"
+            sx={{ width: "100%", bgcolor: theme.palette.primary.dark }}
+          >
+            Account created successfully
+          </Alert>
+        </Snackbar>
+      </Box>
+    </Box>
+  );
 }
